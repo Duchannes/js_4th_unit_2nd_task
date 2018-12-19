d4776e664d463693d105942fe1bbdd42beccb83d
Anton_viarenich <Anton_viarenich@epam.com>
2018-12-19 12:12:17 +0300

    add non angular scenario

--- spec.js
+++ spec.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-undef */
-data = ['Hello', 'abc', '...'];
+const until = protractor.ExpectedConditions;
 
-data.forEach((name) => {
+const dataAngular = ['To Do Homework', 'Hello', 'abc', '...'];
+const dataNonAngular = [
+  { name: `Dirk Gently's Holistic Detective Agency`, date: 2016 },
+  { name: `Westworld`, date: 2016 }
+];
+
+dataAngular.forEach((name) => {
   describe('angular test', function () {
     const addNewTaskField = element(by.css('input[placeholder]'));
     const allNotes = $$('.todo-container');
@@ -43,6 +49,7 @@ data.forEach((name) => {
       addNewNote(name);
       expect(allNotes.count()).toBe(numOfNotes + 1);
       searchNote();
+      browser.sleep(15000);
       expect(allNotes.count()).toBe(1);
     });
 
@@ -73,6 +80,37 @@ data.forEach((name) => {
       searchField.sendKeys('a', protractor.Key.BACK_SPACE);
       comleteTaskBox.click();
       expect(allNotes.count()).toBe(numOfCompletedNotes);
+    });
+  });
+});
+
+dataNonAngular.forEach((data) => {
+  describe('non-angular test', function () {
+    const searchField = browser.$('.headblock-search-txt');
+    const seriesLink = browser.element(by.linkText(data.name));
+    const firstSeason = browser.element(by.partialLinkText(`1 сезон`));
+    const date = element(by.cssContainingText('.pgs-sinfo_list', ' Вышел: ')).element(by.css(`span`));
+
+    jasmine.getEnv().defaultTimeoutInterval = 200000;
+
+    beforeEach(function () {
+      browser.waitForAngularEnabled(false); // For non-angular sites
+      browser.get('http://seasonvar.ru/');
+    });
+
+    it('should have a title', function () {
+      expect(browser.getTitle()).toEqual('Сериалы ТУТ! Сериалы онлайн смотреть бесплатно. Смотреть онлайн');
+    });
+
+    it('find tvSeries', async function () {
+      searchField.sendKeys(data.name, protractor.Key.ENTER);
+      browser.wait(until.elementToBeClickable(seriesLink), 5000);
+      seriesLink.click();
+      browser.wait(until.elementToBeClickable(firstSeason), 5000);
+      firstSeason.click();
+      expect(browser.getTitle()).toContain(data.name);
+      expect(browser.getTitle()).toContain(`1 сезон`);
+      expect(date.getText()).toBe(data.date.toString());
     });
   });
 });
